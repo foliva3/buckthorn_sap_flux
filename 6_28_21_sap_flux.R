1a1a0b9a089f872ee26d9987f36cc58c03931248
foliva3 <fatimaoliva1217696@gmail.com>
2021-07-14 14:26:12 -0400

    added updates from this morning

--- 6_28_21_sap_flux.R
+++ 6_28_21_sap_flux.R
@@ -15,4 +15,38 @@ ggplot(data = tabledt, aes(datef, dT2))+
   geom_path()
 ggplot(data = tabledt, aes(datef, dT16))+
   geom_path()
-help("plot")
+
+#sapwood depth equation
+sensors <- read.csv("K://Environmental_Studies/hkropp/Data/campus/buckthorn/sapflux/sensors_meta.csv")
+ash <- sensors[sensors$Type =="Ash", ]
+ash$sd.cm <- -36.33+(44.28*(1-exp(-0.1306*ash$DBH.cm)))
+#all ash trees have a sapwood depth greater than 3cm so no additional calculations need to be made
+
+tabledt$doy <- yday(tabledt$date)
+tabledt$hour <- hour(tabledt$date)+(minute(tabledt$date)/60)
+
+dtAll <- data.frame(date= rep(tabledt$date, times = 16), 
+                    doy = rep(tabledt$doy, times = 16),
+                    hour = rep(tabledt$hour, times = 16),
+                    sensor = rep(seq(1,16), each = nrow(tabledt)), 
+                    dT = c(tabledt[,3],
+                           tabledt[,4],
+                           tabledt[,5],
+                           tabledt[,6],
+                           tabledt[,7],
+                           tabledt[,8],
+                           tabledt[,9],
+                           tabledt[,10],
+                           tabledt[,11],
+                           tabledt[,12],
+                           tabledt[,13],
+                           tabledt[,14],
+                           tabledt[,15],
+                           tabledt[,16],
+                           tabledt[,17],
+                           tabledt[,18]))
+
+night <- dtAll[dtAll$hour <5|dtAll$hour>=22,]
+maxnight <- aggregate(night$dT, by = list(sensor = night$sensor, 
+                                          doy = night$doy), 
+                      FUN = "max")
