515d854cdddc7e853ea6fa81330ad923112c6b4c
foliva3 <fatimaoliva1217696@gmail.com>
2021-07-20 09:53:15 -0400

    Update 6_28_21_sap_flux.R

--- 6_28_21_sap_flux.R
+++ 6_28_21_sap_flux.R
@@ -1,17 +1,17 @@
 library(lubridate)
 library(ggplot2)
 #read in data
-dats <- read.csv("K:\\Environmental_Studies\\hkropp\\Data\\campus\\buckthorn\\sapflux\\campbell\\06_25_2021\\Sapflow_TableDT.dat",
+dats <- read.csv("K:\\Environmental_Studies\\hkropp\\Data\\campus\\buckthorn\\sapflux\\campbell\\07_16_2021\\Sapflow_TableDT.dat",
                  skip = 4, header = FALSE, na.strings = "NAN")
-datNames <- read.csv("K:\\Environmental_Studies\\hkropp\\Data\\campus\\buckthorn\\sapflux\\campbell\\06_25_2021\\Sapflow_TableDT.dat", 
+datNames <- read.csv("K:\\Environmental_Studies\\hkropp\\Data\\campus\\buckthorn\\sapflux\\campbell\\07_16_2021\\Sapflow_TableDT.dat", 
                      skip= 1, nrows= 2, header= FALSE)
 tabledt <- dats[,1:18]
 colnames(tabledt) <- c("date", "record", paste0("dT", seq(1, 16)))
 plot(tabledt$record, tabledt$dT5, type = "l")
 tabledt$datef <- ymd_hms(tabledt$date)
-ggplot(data = tabledt, aes(datef, dT1))+
+ggplot(data = tabledt, aes(datef, dT9))+
   geom_path()
-ggplot(data = tabledt, aes(datef, dT2))+
+ggplot(data = tabledt, aes(datef, dT10))+
   geom_path()
 ggplot(data = tabledt, aes(datef, dT16))+
   geom_path()
